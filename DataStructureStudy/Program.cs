fb8779677abad3fe756868af0249c4183ddb5ebf
김용오 <gmumdev@gmail.com>
2026-03-24 15:32:12 +0900

    첫 커밋

---
+++ DataStructureStudy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureStudy
+{
+	/// <summary>
+	/// 필수 데이터 클래스 : 스택, 큐
+	/// 타입: Integer
+	/// 제한사항: 배열만 사용
+	/// 
+	/// 
+	/// </summary>
+	public class Stack
+	{
+		public void Add(int data) { }
+
+	}
+	public class Queue
+	{
+
+	}
+	public class LinkedList
+	{
+
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
